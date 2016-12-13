4e524156526e437194ee8d6b42cfed69db3323ac
LukaszDell <ruminski.lukasz@gmail.com>
2016-12-13 21:21:20 +0100

    update 13.12 - contact section

--- app/core/pages/contact/contact.js
+++ app/core/pages/contact/contact.js
@@ -1,21 +1,23 @@
-function ContactController () {
+function ContactController(NgMap) {
     this.app = require('index.json');
-    (function(NgMap) {
-        NgMap.getMap().then(function(map) {
-            console.log(map.getCenter());
-            console.log('markers', map.markers);
-            console.log('shapes', map.shapes);
-        });
-    }())
+
+    var vm = this;
+    NgMap.getMap().then(function (map) {
+        vm.map = map;
+    });
+    vm.customIcon = {
+        "url": "assets/placeholders/GD_logo_menu.png#white",
+        // "path": "M22-48h-44v43h16l6 5 6-5h16z",
+        "scaledSize": [32, 32]
+    };
 }
 module.exports = function(){
     require('./contact.scss');
-    require('ng-map.min.js');
 
     return {
         controller: ContactController,
         controllerAs: 'contact',
         templateUrl: './core/pages/contact/contact.html'
     };
-}
+};
 
